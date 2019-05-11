7abac3a4644771e3b53ea0df89dac22c1726cecc
rx5842 <ryan8xia@gmail.com>
2019-05-11 13:51:13 -0700

    init

--- python3/example_ai.py
+++ python3/example_ai.py
@@ -9,18 +9,35 @@ game = Colorfight()
 
 # Connect to the server. This will connect to the public room. If you want to
 # join other rooms, you need to change the argument
-game.connect(room = 'public')
+game.connect(room = 'constant-run1')
 
 # game.register should return True if succeed.
 # As no duplicate usernames are allowed, a random integer string is appended
 # to the example username. You don't need to do this, change the username
 # to your ID.
 # You need to set a password. For the example AI, the current time is used
-# as the password. You should change it to something that will not change 
-# between runs so you can continue the game if disconnected.
-if game.register(username = 'ExampleAI' + str(random.randint(1, 100)), \
+# as the password. You should change it to something that will not change
+# between runs so you can continue the game if disconnected
+
+def optimizeBld (cell):
+    e = cell.natural_energy
+    g = cell.natural_gold
+
+    if(g<3 and e <3):
+        return BLD_FORTRESS
+    if(g>(e+2)):
+        return BLD_GOLD_MINE
+    else:
+        return BLD_ENERGY_WELL
+
+
+
+if game.register(username = 'ExampleAIRX' + str(random.randint(1, 100)), \
         password = str(int(time.time()))):
     # This is the game loop
+
+
+
     while True:
         # The command list we will send to the server
         cmd_list = []
@@ -28,12 +45,12 @@ if game.register(username = 'ExampleAI' + str(random.randint(1, 100)), \
         my_attack_list = []
         # update_turn() is required to get the latest information from the
         # server. This will halt the program until it receives the updated
-        # information. 
-        # After update_turn(), game object will be updated.   
+        # information.
+        # After update_turn(), game object will be updated.
         game.update_turn()
 
         # Check if you exist in the game. If not, wait for the next round.
-        # You may not appear immediately after you join. But you should be 
+        # You may not appear immediately after you join. But you should be
         # in the game after one round.
         if game.me == None:
             continue
@@ -52,7 +69,7 @@ if game.register(username = 'ExampleAI' + str(random.randint(1, 100)), \
                 # We also try to keep our cell number under 100 to avoid tax
                 if c.attack_cost < me.energy and c.owner != game.uid \
                         and c.position not in my_attack_list \
-                        and len(me.cells) < 95:
+                        and len(me.cells) < 300:
                     # Add the attack command in the command list
                     # Subtract the attack cost manually so I can keep track
                     # of the energy I have.
@@ -65,7 +82,7 @@ if game.register(username = 'ExampleAI' + str(random.randint(1, 100)), \
 
             # If we can upgrade the building, upgrade it.
             # Notice can_update only checks for upper bound. You need to check
-            # tech_level by yourself. 
+            # tech_level by yourself.
             if cell.building.can_upgrade and \
                     (cell.building.is_home or cell.building.level < me.tech_level) and \
                     cell.building.upgrade_gold < me.gold and \
@@ -74,15 +91,16 @@ if game.register(username = 'ExampleAI' + str(random.randint(1, 100)), \
                 print("We upgraded ({}, {})".format(cell.position.x, cell.position.y))
                 me.gold   -= cell.building.upgrade_gold
                 me.energy -= cell.building.upgrade_energy
-                
+
             # Build a random building if we have enough gold
             if cell.owner == me.uid and cell.building.is_empty and me.gold >= 100:
                 building = random.choice([BLD_FORTRESS, BLD_GOLD_MINE, BLD_ENERGY_WELL])
+                building = optimizeBld(cell);
                 cmd_list.append(game.build(cell.position, building))
                 print("We build {} on ({}, {})".format(building, cell.position.x, cell.position.y))
                 me.gold -= 100
 
-        
+
         # Send the command list to the server
         result = game.send_cmd(cmd_list)
         print(result)
